67eafa5b6fb4990f7bfb97d35bbceee6539e284a
anoop142 <anoop.skumar1507@gmail.com>
2022-08-09 19:03:32 +0530

    Add option to serve a single file * Add verbose flag

--- main.go
+++ main.go
@@ -10,7 +10,16 @@ import (
 	"net"
 	"net/http"
 	"path/filepath"
+	"os"
+	"net/url"
 )
+
+
+type config struct{
+	path string
+	port string
+	verbose bool
+}
 
 func getLocalIP() string {
 	addrs, err := net.InterfaceAddrs()
@@ -29,20 +38,62 @@ func getLocalIP() string {
 	return ""
 }
 
+func (cfg *config)serveFile(w http.ResponseWriter, r *http.Request){
+	if cfg.verbose{
+		log.Println(r.URL.Path)
+	}
+	if r.URL.Path != "/" + filepath.Base(cfg.path){
+		http.NotFound(w,r)
+		return
+	}
+	http.ServeFile(w, r, cfg.path)
+}
+
+
+func (cfg *config) serveDir(w http.ResponseWriter, r *http.Request){
+	http.FileServer(http.Dir("/tmp"))
+}
+
 func main() {
+	var cfg config
+
 	flag.Usage = func() {
 		fmt.Println("sfserver - A simple static file server by Anoop")
 		flag.PrintDefaults()
 	}
 
-	port := flag.String("p", "8000", "port to serve on")
-	directory := flag.String("d", ".", "the directory to serve")
+	flag.StringVar(&cfg.port ,"p", "8000", "port to serve on")
+	flag.StringVar(&cfg.path ,"f", ".", "file or directory to serve")
+	flag.BoolVar(&cfg.verbose, "v", false, "verbose")
+
 	flag.Parse()
 
-	absolutePath, _ := filepath.Abs(*directory)
+	absolutePath, _ := filepath.Abs(cfg.path)
 	localIP := getLocalIP()
 
-	http.Handle("/", http.FileServer(http.Dir(absolutePath)))
-	log.Printf("Serving %s on %s:%s\n", absolutePath, localIP, *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	fileInfo, err := os.Stat(cfg.path)
+
+	if err != nil{
+		log.Fatal(err)
+	}
+	
+	if fileInfo.IsDir(){
+		http.Handle("/", http.FileServer(http.Dir(absolutePath)))
+		if cfg.verbose{
+			log.Printf("Serving %s on %s:%s\n", absolutePath, localIP, cfg.port)
+		}else{
+			fmt.Printf("%s:%s\n", localIP, cfg.port)
+		}
+	}else{
+		http.HandleFunc("/", cfg.serveFile)
+		encodedFileName := url.PathEscape(filepath.Base(cfg.path))
+
+		if cfg.verbose{
+			log.Printf("Serving %s:%s/%s\n", localIP, cfg.port, encodedFileName)
+		}else{
+			fmt.Printf("%s:%s/%s\n", localIP, cfg.port, encodedFileName)
+		}
+	}
+
+	log.Fatal(http.ListenAndServe(":"+cfg.port, nil))
 }
